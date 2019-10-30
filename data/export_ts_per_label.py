5bb1736028cb273f85dbc7452838cb53eb8fb66f
Bock  Christian <chrs.bock@gmail.com>
2019-10-30 12:56:09 +0100

    Adjusted almost running tex code

--- data/export_ts_per_label.py
+++ data/export_ts_per_label.py
@@ -1,10 +1,11 @@
 from utilities import get_ucr_dataset
 import numpy as np
+import pandas as pd
 
-dataset_name = 'ItalyPowerDemand'
+dataset_name = 'FacesUCR'
 X_train, y_train, X_test, y_test = get_ucr_dataset('./UCR', dataset_name)
 
 for cl in np.unique(y_train):
     indices = np.where(y_train == cl)
     ts = X_train[indices]
-    np.savetxt(f"{dataset_name}_{int(cl)}.csv", ts.transpose())
+    pd.DataFrame(ts.transpose()).to_csv(f"{dataset_name}_{int(cl)}.csv", index_label='ind')
