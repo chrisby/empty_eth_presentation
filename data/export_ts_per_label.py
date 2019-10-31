420dfa028f772c5d57eae7a5cd6aa89e7bb3ead1
Christian Bock <chrs.bock@gmail.com>
2019-10-31 11:10:34 +0100

    Stuff

--- data/export_ts_per_label.py
+++ data/export_ts_per_label.py
@@ -1,11 +1,21 @@
 from utilities import get_ucr_dataset
 import numpy as np
 import pandas as pd
+import argparse
 
-dataset_name = 'FacesUCR'
-X_train, y_train, X_test, y_test = get_ucr_dataset('./UCR', dataset_name)
+def main(args):
 
-for cl in np.unique(y_train):
-    indices = np.where(y_train == cl)
-    ts = X_train[indices]
-    pd.DataFrame(ts.transpose()).to_csv(f"{dataset_name}_{int(cl)}.csv", index_label='ind')
+    dataset_name = args.dataset
+    X_train, y_train, X_test, y_test = get_ucr_dataset('./UCR', dataset_name)
+
+    for cl in np.unique(y_train):
+        indices = np.where(y_train == cl)
+        ts = X_train[indices]
+        pd.DataFrame(ts.transpose()).to_csv(f"{dataset_name}_{int(cl)}.csv", index_label='ind')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dataset', type=str, help="Name of data set")
+
+    args = parser.parse_args()
+    main(args)
